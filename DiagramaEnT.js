b03e2cb90d421fc911cda63f64d5f4fb2c2575d2
xsoulwithrage <101188188+xsoulwithrage@users.noreply.github.com>
2022-04-12 11:01:40 -0400

    Add files via upload

---
+++ DiagramaEnT.js
@@ -0,0 +1,56 @@
+* /
+Fragmento de código 1
+    *
+    /
+
+function muestraInformaciónContacto() {
+    var auntContactInfo = ["Paula", "Smith", "Calle Principal 1234", "St. Louis", "MO", 12345];
+    console.log(auntContactInfo);
+}
+
+*
+/
+Variables Datos
+Produce = ["Paula", "Smith", "Calle Principal 1234", "St. Louis", "MO", 12345];
+
+*
+/
+
+*
+/
+Fragmento de código 2
+    *
+    /
+
+function muestraListaDeCompras() {
+    var produce = ["manzanas", "naranjas"];
+    var frozen = ["brócoli", "helado"];
+    frozen.push("croqueta de papa");
+    console.log(frozen);
+}
+
+*
+/
+Variables Datos
+Produce = ["manzanas", "naranjas"];
+
+*
+/
+
+*
+/
+Fragmento de código 3
+    *
+    /
+var movieLibrary = ["Bambi", "E.T.", "Toy Story"];
+movieLibrary.push("Zoro");
+movieLibrary[1] = "Beetlejuice";
+console.log(movieLibrary);
+
+*
+/
+Variables Datos
+Produce = ["Bambi", "E.T.", "Toy Story"];
+
+*
+/
